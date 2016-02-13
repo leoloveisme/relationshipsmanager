b23c4c68124a08b04849898cb2ddc8e020be5951
leozukov <leozukov@gmail.com>
2016-02-13 11:05:18 +0100

    Added some new files to the project and uploading some new changes. 13. February 2016

---
+++ footer.php
@@ -0,0 +1,8 @@
+<table width=100%>
+<tr>
+<td align=right>
+<a href="#top" title="Jump to the top" target="">Jump to the top</a> 
+</td>
+</tr>
+</table>
+<br>
---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+		<title>Relationshipsmanager</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	</head>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script> -->
+</head>
+<body>
+<p><a name="top"></a> <?php include ("header.php"); ?></p>
+
+<p>&nbsp;</p>
+
+<table width=100%>
+<tr>
+<td width=20%>
+&nbsp;
+</td>
+<td width=5%>&nbsp;</td>
+<td width=50%>
+&nbsp;
+</td>
+<td width=5%>&nbsp;</td>
+<td width=20%>
+&nbsp;
+</td>
+</tr>
+</table>
+<?php include("footer.php"); ?>
+
+<p>&nbsp;</p>
+</body>
+</html>
