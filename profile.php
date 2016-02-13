c0a2febee083b17bb2a637c490838f5a0b85a5ba
leozukov <leozukov@gmail.com>
2016-02-13 15:24:38 +0100

    Uploading some new changes to several files and created couple of new files. 13. February 2016

---
+++ profile.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+		<title>Relationshipsmanager</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	</head>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script> -->
+</head>
+<body>
+<h1>Profile</h1>
+<br><br>
+Вася Пупкин <font color=gray>Visible to all</font>
+<br><br>
+E-Mail: <font color=gray>Visible to all</font>
+<br><br>
+vasya.pupkin@pupkin.com
+<br><br>
+Telefon: <font color=gray>Visible only to friends</font>
+<br><br>
++ 49 175 4523 6543
+<br><br>
+Date of birth: <font color=gray>Visible to all</font>
+<br><br>
+15 апреля 1990 
+<br><br>
+Notes: <font color=gray>Visible to nobody / Private</font>
+<br><br>
+Text of the note(s)
+<br><br>
+<a name="settings"></a><h2>Settings</h2>
+</body>
+</html>
